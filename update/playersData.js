d0bb519e2cd0416911ac94c9e6bd404fb78ce4e6
Michael R. Schmidt <mikenging@hotmail.com>
2023-10-24 11:46:18 -0700

    automatic map changes

---
+++ update/playersData.js
@@ -0,0 +1,152 @@
+// NOTE: Please only modify player attributes below such as name, color, and visible
+// To hide a player's marker, change their 'visible' variable from 'true' to 'false'
+// This file is automatically updated, and if other parts are changed, it will fail to update
+// Changes will only be read from the /map/playersData.js file - /update/playersData.js will be overwritten, so don't make changes there
+var playersData = {
+  "players": [
+    {
+      "uuid": "312331e0-0ae9-47d8-87ba-616a40569ae9",
+      "name": "Player 31233",
+      "dimensionId": 0,
+      "position": [
+        1063.0603,
+        57.62001,
+        -358.7
+      ],
+      "color": "Gold",
+      "visible": true
+    },
+    {
+      "uuid": "3f57698e-3afd-4323-ac43-40af2108fba4",
+      "name": "Player 3f576",
+      "dimensionId": 0,
+      "position": [
+        986.2306,
+        68.68251,
+        -413.03616
+      ],
+      "color": "SlateGray",
+      "visible": true
+    },
+    {
+      "uuid": "58d1caf8-b5f3-43f2-b18e-8dad407dc989",
+      "name": "Player 58d1c",
+      "dimensionId": 0,
+      "position": [
+        3462.8972,
+        140.62001,
+        4394.3
+      ],
+      "color": "SlateGray",
+      "visible": true
+    },
+    {
+      "uuid": "6675e6c3-4028-4c54-b88a-d3a261465feb",
+      "name": "Player 6675e",
+      "dimensionId": 0,
+      "position": [
+        1275.5,
+        103.62001,
+        301.5
+      ],
+      "color": "DarkBlue",
+      "visible": true
+    },
+    {
+      "uuid": "906192aa-eee2-4e2f-ad91-c51e14f9beba",
+      "name": "Player 90619",
+      "dimensionId": 0,
+      "position": [
+        949.9415,
+        72.55751,
+        -403.64856
+      ],
+      "color": "Teal",
+      "visible": true
+    },
+    {
+      "uuid": "a1b1a8ca-e350-4784-a3ae-ab7f840afbe9",
+      "name": "Player a1b1a",
+      "dimensionId": 0,
+      "position": [
+        989.5,
+        68.62001,
+        -410.5
+      ],
+      "color": "SaddleBrown",
+      "visible": true
+    },
+    {
+      "uuid": "b4c7ca86-a540-4117-80cd-928f6844125d",
+      "name": "Player b4c7c",
+      "dimensionId": 0,
+      "position": [
+        242.79391,
+        68.62001,
+        140.17099
+      ],
+      "color": "SlateGray",
+      "visible": true
+    },
+    {
+      "uuid": "b6bead80-e068-4631-8d99-cc5514b763d9",
+      "name": "Player b6bea",
+      "dimensionId": 0,
+      "position": [
+        1260.5424,
+        76.18251,
+        298.79007
+      ],
+      "color": "SlateGray",
+      "visible": true
+    },
+    {
+      "uuid": "b7e71fba-7168-4996-ad29-9893194ceec4",
+      "name": "Player b7e71",
+      "dimensionId": 0,
+      "position": [
+        266.82504,
+        68.55751,
+        155.27785
+      ],
+      "color": "Gold",
+      "visible": true
+    },
+    {
+      "uuid": "be64bea4-8aee-4943-8837-ce9e03ab2efc",
+      "name": "Player be64b",
+      "dimensionId": 0,
+      "position": [
+        232.5,
+        28.62001,
+        350.5
+      ],
+      "color": "DarkRed",
+      "visible": true
+    },
+    {
+      "uuid": "d79ec26b-75c9-4097-894b-8b1e320a57ab",
+      "name": "Player d79ec",
+      "dimensionId": 0,
+      "position": [
+        227.40677,
+        28.62001,
+        346.13602
+      ],
+      "color": "DarkOrange",
+      "visible": true
+    },
+    {
+      "uuid": "dda82389-4ee0-43db-97c6-58c23e1bc275",
+      "name": "Player dda82",
+      "dimensionId": 0,
+      "position": [
+        272.38962,
+        67.55751,
+        160.16022
+      ],
+      "color": "DeepPink",
+      "visible": true
+    }
+  ]
+};
